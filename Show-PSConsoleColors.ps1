81ef74ae6a3a4a1a78a159d3566879fd50f2f40e
Kevin A. Pieckiel <going.vague@simplelogin.co>
2023-02-18 13:25:23 -0500

    Turn PSConsoleColors code into a function

--- Show-PSConsoleColors.ps1
+++ Show-PSConsoleColors.ps1
@@ -1,29 +1,31 @@
-<#
-.SYNOPSIS
-Display a PowerShell console color table.
+function Show-PSConsoleColors {
+    <#
+    .SYNOPSIS
+    Display a PowerShell console color table.
 
-.DESCRIPTION
-Displays a table that demonstrates all the text colors available in
-the PowerShell console.
+    .DESCRIPTION
+    Displays a table that demonstrates all the text colors available in
+    the PowerShell console.
 
-.INPUTS
-This script does not take pipelined inputs.
+    .INPUTS
+    This script does not take pipelined inputs.
 
-.OUTPUTS
-This script does not output any objects.
+    .OUTPUTS
+    This script does not output any objects.
 
-.NOTES
-A 136-character windows width is required to properly display the table.
+    .NOTES
+    A 136-character windows width is required to properly display the table.
 
-.LINK
-https://stackoverflow.com/a/41954792/5344665
-#>
+    .LINK
+    https://stackoverflow.com/a/41954792/5344665
+    #>
 
-$colors = [enum]::GetValues([System.ConsoleColor])
-ForEach ($bgcolor in $colors) {
-    ForEach ($fgcolor in $colors) {
-		Write-Host "$fgcolor|" -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine
-	}
-	
-    Write-Host " on $bgcolor"
+    $colors = [enum]::GetValues([System.ConsoleColor])
+    ForEach ($bgcolor in $colors) {
+        ForEach ($fgcolor in $colors) {
+            Write-Host "$fgcolor|" -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine
+        }
+        
+        Write-Host " on $bgcolor"
+    }
 }
