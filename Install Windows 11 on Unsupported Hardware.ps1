e7dcb93e37c4b78c72a3930c20c07d6e6e908b7d
Kevin A. Pieckiel <kevin.pieckiel@gmail.com>
2024-01-16 18:30:38 -0500

    Streamline PowerShell registry PowerShell script
    Registry data is now stored as CSV data that is looped through and
    applied to the registry.

--- Install Windows 11 on Unsupported Hardware.ps1
+++ Install Windows 11 on Unsupported Hardware.ps1
@@ -3,26 +3,27 @@
 # "BypassSecureBootCheck"=dword:00000001
 # "BypassRAMCheck"=dword:00000001
 
-# Test registry path
-$RegistryPath = 'HKLM:\SYSTEM\Setup\LabConfig'
-if (-NOT (Test-Path $RegistryPath)) {
-  New-Item -Path $RegistryPath -Force | Out-Null
-}  
 
-# Set keys
-New-ItemProperty -Path $RegistryPath -Name 'BypassTPMCheck' -Value 1 -PropertyType DWord -Force
-New-ItemProperty -Path $RegistryPath -Name 'BypassSecureBootCheck' -Value 1 -PropertyType DWord -Force
-New-ItemProperty -Path $RegistryPath -Name 'BypassRAMCheck' -Value 1 -PropertyType DWord -Force
+# Put registry data here as CSV text.
+$CsvKeys = 'Key,Value,Type,Data
+HKLM:\SYSTEM\Setup\LabConfig,BypassTPMCheck,DWord,1
+HKLM:\SYSTEM\Setup\LabConfig,BypassSecureBootCheck,DWord,1
+HKLM:\SYSTEM\Setup\LabConfig,BypassRAMCheck,DWord,1
+'
 
+# Convert the CSV text to something usable and extract the unique paths.
+$Keys = $CsvKeys | ConvertFrom-Csv
+$Paths = $Keys.Key | Sort-Object -Unique
 
-# [HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup]
-# "AllowUpgradeWithUnsupportedTPMOrCPU"=dword:00000001
+# Make sure all of the paths exist, and create them if they don't.
+$Paths | ForEach-Object -Process {
+	if (-not (Test-Path -Path $_)) {
+	  New-Item -Path $_ -Force | Out-Null
+	}
+}
 
-# Test registry path
-$RegistryPath = 'HKLM:\SYSTEM\Setup\MoSetup'
-if (-NOT (Test-Path $RegistryPath)) {
-  New-Item -Path $RegistryPath -Force | Out-Null
-}  
-
-# Set keys
-New-ItemProperty -Path $RegistryPath -Name 'AllowUpgradeWithUnsupportedTPMOrCPU' -Value 1 -PropertyType DWord -Force
+# Create the properties and values.
+$Keys | ForEach-Object -Process {
+	New-ItemProperty -Path $_.Key -Name $_.Value -Value $_.Data `
+		-PropertyType $_.Type -Force | Out-Null
+}
